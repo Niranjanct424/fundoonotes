3897c44238fd84307b1bb19796e41f24183fbc54
Niranjanct424 <niruct5@gmail.com>
2020-03-02 19:34:59 +0530

    [Niranjan] created NoteRepositoryInterface

--- src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/NoteRepository.java
@@ -1,5 +1,41 @@
 package com.bridgelabz.fundoonotes.repository;
 
-public class NoteRepository {
+import java.util.List;
 
+import com.bridgelabz.fundoonotes.model.Note;
+
+public interface NoteRepository {
+
+	/**
+	 * @Desciption:this method takes Note as input parameter stores note in database
+	 * @param newNote as input parameter
+	 * @return Note object
+	 */
+	public Note saveOrUpdate(Note newNote);
+
+	/**
+	 * @Desciption:this method takes noteId as input parameter and gives the note 
+	 * @param newNote as input parameter
+	 * @return Note object
+	 */
+	public Note getNote(long noteId);
+
+	
+	public boolean isDeletedNote(long noteId);
+
+
+	public List<Note> getAllNotes(long userId);
+
+	
+	public List<Note> getAllTrashedNotes(long userId);
+
+	
+	
+	public List<Note> getAllPinnedNotes(long userId);
+
+
+	public List<Note> getAllArchivedNotes(long userId);
+
+	
+	public List<Note> searchBy(String noteTitle);
 }
