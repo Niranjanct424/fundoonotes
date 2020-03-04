3762afa42911146d3803eac136c9af7d92fcca11
Niranjanct424 <niruct5@gmail.com>
2020-03-04 19:28:53 +0530

    [Niranjan] 4/3/2020 woring on Note Service implementation

--- src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -17,7 +17,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.bridgelabz.fundoo.Entity.UserInformation;
 import com.bridgelabz.fundoonotes.dto.UpdatePassword;
 import com.bridgelabz.fundoonotes.model.User;
 
--- src/main/java/com/bridgelabz/fundoonotes/service/NoteServiceImplementation.java
+++ src/main/java/com/bridgelabz/fundoonotes/service/NoteServiceImplementation.java
@@ -1,59 +1,161 @@
 package com.bridgelabz.fundoonotes.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bridgelabz.fundoonotes.dto.NoteDto;
 import com.bridgelabz.fundoonotes.dto.RemainderDto;
+import com.bridgelabz.fundoonotes.exception.AuthorizationException;
+import com.bridgelabz.fundoonotes.exception.UserException;
 import com.bridgelabz.fundoonotes.model.Note;
+import com.bridgelabz.fundoonotes.model.User;
+import com.bridgelabz.fundoonotes.repository.NoteRepository;
+import com.bridgelabz.fundoonotes.repository.UserRepository;
+import com.bridgelabz.fundoonotes.utility.JWTToken;
 
 public class NoteServiceImplementation implements AbstractNoteService {
 
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private NoteRepository noteRepository;
+	@Autowired
+	private JWTToken jwtToken;
+
+	@SuppressWarnings("unused")
+	private User authenticatedUser(String token) {
+		com.bridgelabz.fundoonotes.model.User fetchedUser = userRepository.getUser(jwtToken.decodeToken(token));
+		if (fetchedUser != null) {
+			return fetchedUser;
+		}
+		throw new AuthorizationException("Authorization faild", 401);
+	}
+
+	@SuppressWarnings("unused")
+	private Note verifiedNote(long noteId) {
+		Note fetchedNote = noteRepository.getNote(noteId);
+		if (fetchedNote != null) {
+			return fetchedNote;
+		}
+		throw new UserException("Note Not Found", 404);
+	}
+
 	@Override
 	public boolean createNote(NoteDto noteDto, String token) {
-		// TODO Auto-generated method stub
-		return false;
+
+		User fetchedUser = authenticatedUser(token);
+
+		if (fetchedUser != null) {
+			Note newNote = new Note();
+			BeanUtils.copyProperties(noteDto, newNote);
+			newNote.setCreatedDate(LocalDateTime.now());
+			newNote.setColor("white");
+			fetchedUser.getNote().add(newNote);
+			noteRepository.saveOrUpdate(newNote);
+			return true;
+		}
+		throw new AuthorizationException("Authorization faild", 401);
 	}
 
 	@Override
 	public boolean updateNote(NoteDto noteDto, long noteId, String token) {
-		// TODO Auto-generated method stub
+		// using token we will get the user
+		authenticatedUser(token);
+		// here we get the existing note
+		Note fetchedNote = verifiedNote(noteId);
+		BeanUtils.copyProperties(noteDto, fetchedNote);
+		fetchedNote.setUpdatedDate(LocalDateTime.now());
+		noteRepository.saveOrUpdate(fetchedNote);
 		return false;
 	}
 
+	/**
+	 * using token we will get the user and note is verified using note id then note
+	 * will deleted using noteId.
+	 */
 	@Override
 	public boolean deleteNote(long noteId, String token) {
-		// TODO Auto-generated method stub
-		return false;
+		authenticatedUser(token);
+		verifiedNote(noteId);
+		noteRepository.isDeletedNote(noteId);
+		return true;
 	}
 
+	/**
+	 * if we found found authorized user then we will verify the Note if the user
+	 * note is not archived will archieveNote or its archived before only.
+	 */
 	@Override
 	public boolean archieveNote(long noteId, String token) {
-		// TODO Auto-generated method stub
+		authenticatedUser(token);
+		Note fetchedNote = verifiedNote(noteId);
+		// fetched note is not archived
+		if (!fetchedNote.isArchived()) {
+			fetchedNote.setArchived(true);
+			fetchedNote.setUpdatedDate(LocalDateTime.now());
+			noteRepository.saveOrUpdate(fetchedNote);
+			return true;
+		}
 		return false;
 	}
 
+	/**
+	 * We will make the the note pinned if not already pinned 
+	 * we will make it un pinned in the second time of service called. 
+	 */
 	@Override
 	public boolean isPinnedNote(long noteId, String token) {
-		// TODO Auto-generated method stub
+		authenticatedUser(token);
+		// verified valid note
+		Note fetchedNote = verifiedNote(noteId);
+		if (!fetchedNote.isPinned()) {
+			fetchedNote.setPinned(true);
+			fetchedNote.setUpdatedDate(LocalDateTime.now());
+			noteRepository.saveOrUpdate(fetchedNote);
+			return true;
+		}
+		// making note Unpinned if note  already pinned 
+		fetchedNote.setPinned(false);
+		fetchedNote.setUpdatedDate(LocalDateTime.now());
+		noteRepository.saveOrUpdate(fetchedNote);
 		return false;
 	}
 
+	/**
+	 * if the note is verified as valid note then will check note is not trashed 
+	 * its set to trashed or  if trashed already returns false
+	 */
 	@Override
 	public boolean trashNote(long noteId, String token) {
-		// TODO Auto-generated method stub
+		authenticatedUser(token);
+		Note fetchedNote = verifiedNote(noteId);
+		if (!fetchedNote.isTrashed()) {
+			fetchedNote.setTrashed(true);
+			fetchedNote.setUpdatedDate(LocalDateTime.now());
+			noteRepository.saveOrUpdate(fetchedNote);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public List<Note> getallNotes(String token) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return noteRepository.getAllNotes(authenticatedUser(token).getUserId());
 	}
 
 	@Override
 	public List<Note> getAllTrashedNotes(String token) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Note> trashedNotes =  noteRepository.getAllTrashedNotes(authenticatedUser(token).getUserId());
+		if(!trashedNotes.isEmpty())
+		{
+			return trashedNotes;
+		}
+		return trashedNotes;
 	}
 
 	@Override
