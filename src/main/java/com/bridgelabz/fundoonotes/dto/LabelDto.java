016d100b715a18c60f54815dfa8cb2e29c24055e
Niranjanct424 <niruct5@gmail.com>
2020-03-07 15:47:59 +0530

    [Niranjan] 7/3/2020 creating AbstractLabelRepository

---
+++ src/main/java/com/bridgelabz/fundoonotes/dto/LabelDto.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.fundoonotes.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class LabelDto {
+	
+	@ApiModelProperty(notes = "Name of the label", name = "label name")
+	private String labelName;
+
+}
